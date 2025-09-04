d09deb1d925b15125305a1fa1024578f41ae940d
Naman Mittal <namanmittal501@gmail.com>
2025-09-04 17:20:13 +0530

    Refactor layout and theme provider, add header component, implement Clerk authentication, and update dependencies

--- app/page.js
+++ app/page.js
@@ -2,10 +2,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
 return(
   <>
-  <div>Hello World</div>
-   <Button className="mt-6">Naman
-
-   </Button>
+  <Button>YOOOOO</Button>
    </>
 )  
 }
