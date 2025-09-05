d52d0ae0d837c2fef463985bcdb87be901e52ceb
Naman Mittal <namanmittal501@gmail.com>
2025-09-06 00:17:15 +0530

    feat: add user management with Clerk and Prisma integration
    - Implemented checkUser function to manage user authentication and creation in the database.
    - Created prisma.js for Prisma Client initialization.
    - Updated next.config.mjs to allow images from additional domains.
    - Added @prisma/client and prisma dependencies to package.json and package-lock.json.
    - Created initial database migration for User, Assessment, Resume, CoverLetter, and IndustryInsight models.
    - Defined schema.prisma with models and relationships for user data management.

--- data/testimonial.js
+++ data/testimonial.js
@@ -1,25 +1,30 @@
 export const testimonial = [
-  {
-    quote:
-      "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
-    author: "Sarah Chen",
-    image: "https://randomuser.me/api/portraits/women/75.jpg",
-    role: "Software Engineer",
-    company: "Tech Giant Co.",
-  },
-  {
+    {
     quote:
       "The industry insights helped me pivot my career successfully. The salary data was spot-on!",
-    author: "Michael Rodriguez",
-    image: "https://randomuser.me/api/portraits/men/75.jpg",
+    author: "Chussu Sehghal",
+    // image: "https://randomuser.me/api/portraits/men/75.jpg",
+    image:"https://www.juit.ac.in/profile_pics/juit_faculty_profilec3f1620ee4.jpg",
     role: "Product Manager",
     company: "StartUp Inc.",
   },
+  
+  {
+    quote:
+      "The AI-powered interview prep was a game-changer. Landed my dream job at a top tech company!",
+    author: "B. R. Ambedkar",
+    // image: "https://randomuser.me/api/portraits/women/75.jpg",
+    image:"https://www.juit.ac.in/profile_pics/Aayush_cse.jpg",
+    role: "Software Engineer",
+    company: "Tech Giant Co.",
+  },
+
   {
     quote:
       "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
-    author: "Priya Patel",
-    image: "https://randomuser.me/api/portraits/women/74.jpg",
+    author: "Deepak Gupta",
+    // image: "https://randomuser.me/api/portraits/women/74.jpg",
+    image:"https://www.juit.ac.in/profile_pics/deepak_cse.jpg",
     role: "Marketing Director",
     company: "Global Corp",
   },
--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,11 @@ const nextConfig = {
      remotePatterns:[
         {
             protocol:"https",
-            hostname:"randomuser.me"
+            hostname:"randomuser.me" 
+        },
+        {
+            protocol:"https",
+            hostname:"www.juit.ac.in" 
         }
     ]
    }
